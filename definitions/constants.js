2f5d9cfa2b7c69dc87669ae40ef70933daecc22b
Zach Renwick <zach.renwick@gmail.com>
2025-01-04 05:40:35 +0000

    file name changes and descriptions

---
+++ definitions/constants.js
@@ -0,0 +1,4 @@
+const first_date_to_load_data = ["'2021-03-01'"];
+module.exports = {
+    first_date_to_load_data
+};
--- definitions/declarations.js
+++ definitions/declarations.js
@@ -2,7 +2,7 @@
   "ga_sessions_*"
 ].forEach((name) =>
   declare({
-    database: "aritzia-ga", //set your Google Analytics Universal Analytics database/dataset ID here
+    database: "plenary-network-243016", //set your Google Analytics Universal Analytics database/dataset ID here
     schema: "141585386", //this is the Google Analytics UA master property view ID
     name,
   })
@@ -11,8 +11,8 @@
   "events_*"
 ].forEach((name) =>
   declare({
-    database: "atz-data-ingest-prd",//set your Google Analytics 4 database/dataset ID here
-    schema: "analytics_352999334", //this is the Google Analytics 4 master property view ID
+    database: "plenary-network-243016",//set your Google Analytics 4 database/dataset ID here
+    schema: "analytics_307314851", //this is the Google Analytics 4 master property view ID
     name,
   })
 );
