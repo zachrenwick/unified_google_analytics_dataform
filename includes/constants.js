09101cb3169236cf5a54369c5c66545b6c5a2fde
Zach Renwick <zach.renwick@gmail.com>
2025-01-04 22:59:32 +0000

    big changes

--- includes/constants.js
+++ includes/constants.js
@@ -1,8 +1,8 @@
 /*
     Define relevant cutover and first date constants for this dataform project 
 */
-const first_date_to_load_data = ["'2021-03-01'"]; // first date to load GA3/ GA Universal Analytics data
-const ga4_cutover_date = ["'2024-04-15'"]; // first date to load GA4 data, prior day will be last day to load GA UA data
+const first_date_to_load_data = ["'2020-01-01'"]; // first date to load GA3/ GA Universal Analytics data
+const ga4_cutover_date = ["'2023-01-01'"]; // first date to load GA4 data, prior day will be last day to load GA UA data
 module.exports = {
     first_date_to_load_data,
     ga4_cutover_date
